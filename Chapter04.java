8c26f6b19659d5c478486553a19116a3210ee2e7
mokumoku-chan <maho.kumo1997@gmail.com>
2022-05-30 00:07:11 +0900

    Verification Issues chapter04

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String name = "mokumoku-chan";
+    final int Lucky_num = 7;
+
+    System.out.println(name);
+
+    name = "Happy!";
+    System.out.println(name);
+
+    System.out.println(Lucky_num);
+  }
+}
